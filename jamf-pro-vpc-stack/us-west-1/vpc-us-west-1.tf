0058ba72332b7d237e32c7a02d8323309f6b95ee
Szymon Kodrebski <szymon.kodrebski@jamf.com>
2018-08-03 11:30:02 +0200

    fixed terraform variables

--- jamf-pro-vpc-stack/us-west-1/vpc-us-west-1.tf
+++ jamf-pro-vpc-stack/us-west-1/vpc-us-west-1.tf
@@ -2,14 +2,14 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "env_name" {}
 
-vpc_cidr:               10.0.0.0/16
-aws_region:             us-west-1
-availability_zone_1:    us-west-1a
-availability_zone_2:    us-west-1c
-public_subnet_1_cidr:   10.0.0.0/24
-private_subnet_1_cidr:  10.0.1.0/24
-public_subnet_2_cidr:   10.0.2.0/24
-private_subnet_2_cidr:  10.0.3.0/24
+vpc_cidr = "10.0.0.0/16"
+aws_region = "us-west-1"
+availability_zone_1 = "us-west-1a"
+availability_zone_2 = "us-west-1c"
+public_subnet_1_cidr = "10.0.0.0/24"
+private_subnet_1_cidr = "10.0.1.0/24"
+public_subnet_2_cidr = "10.0.2.0/24"
+private_subnet_2_cidr = "10.0.3.0/24"
 
 
 provider "aws" {
