97ac9d443945d02b3f1a4a531ffbf6bd47e776c0
Szymon Kodrebski <szymon.kodrebski@jamf.com>
2018-08-06 12:35:27 +0200

    changed terraform script

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-pwd
+HOME_DIR=$(pwd)
+
 cd ${TERRAFORM_DIRECTORY}
 terraform init
 
-terraform plan --var "env_name=${ENV}" -out ${OUTPUT_DIRECTORY}/plan
+terraform plan --var "env_name=${ENV}" -out ${HOME_DIR}/${OUTPUT_DIRECTORY}/plan
