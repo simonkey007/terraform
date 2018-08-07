cd6aa80b6d615f303dc8e65ef959bf5162c6210b
Szymon Kodrebski <szymon.kodrebski@jamf.com>
2018-08-07 16:18:12 +0200

    added ALB resource

--- jamf-pro-vpc-stack/us-west-1/vpc-us-west-1.tf
+++ jamf-pro-vpc-stack/us-west-1/vpc-us-west-1.tf
@@ -222,7 +222,7 @@ resource "aws_lb" "test" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["${aws_security_group.alb_sg.id}"]
-  subnets            = ["${aws_subnet.public_*.id}"]
+  subnets            = ["${aws_subnet.public_1.id}","${aws_subnet.public_2.id}"]
 
   enable_deletion_protection = true
 
