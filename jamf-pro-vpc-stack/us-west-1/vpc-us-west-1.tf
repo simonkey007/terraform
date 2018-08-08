4d1775029ac60cb1cd9bc348391af44ac489f833
Szymon Kodrebski <szymon.kodrebski@jamf.com>
2018-08-08 17:02:51 +0200

    removed postgres port from ec2 sg

--- jamf-pro-vpc-stack/us-west-1/vpc-us-west-1.tf
+++ jamf-pro-vpc-stack/us-west-1/vpc-us-west-1.tf
@@ -304,13 +304,6 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port = 5432
-    to_port = 5432
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port = 0
     to_port = 0
