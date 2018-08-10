1a8dac2b9213773784286bd15df9e30e2531507f
Szymon Kodrebski <szymon.kodrebski@jamf.com>
2018-08-10 12:04:37 +0200

    added script for destroying stack

---
+++ scripts/destroy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+export TF_IN_AUTOMATION="true"
+
+mkdir ~/.ssh/
+echo -e "${PRIVATE_KEY}" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+HOME_DIR=$(pwd)
+tar -xzf  ${TERRAFORM_S3_DIRECTORY}/terraform.tgz
+cd ${OUTPUT_DIRECTORY}
+terraform destroy -input=false
+
+cd ${HOME_DIR}
+tar -czf terraform.tgz ${OUTPUT_DIRECTORY}
+mv terraform.tgz ${OUTPUT_DIRECTORY}
